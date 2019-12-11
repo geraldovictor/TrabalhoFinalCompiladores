fa7dfa39141b3ed9b40ab5d7454c04d220b614a4
codehg <hugoaraliveira@gmail.com>
2019-12-11 00:00:18 -0300

    Cria e atualiza o htmltags

---
+++ hyperfython/htmltags.py
@@ -0,0 +1,17 @@
+HTMLTAGS = ['p', 'pre','section','div', 'head','title',
+            'html','article','aside','details','span',
+            'small','footer', 'header','main', 'figure',
+            'figcaption', 'h1', 'h2', 'iframe','style',
+            'button','fieldset', 'label', 'legend',
+            'h3', 'h4', 'h5', 'h6', 'span', 'applet',
+            'big', 'center', 'dir',
+            'head', 'footer', 'body', 'body','progress',
+            'samp','summary','time','var',
+            'abbr', 'blockquote', 'code', 
+            'b', 'em', 'del', 'i', 'ins', 'mark',
+            'p', 'sub', 'sup', 'u', 'textarea', 'canvas',
+            'img', 'a', 'bdo', 'address', 'dialog',
+            'cite', 'datalist', 'kbd', 'meter', 'progress',]
+
+CLOSEDHTMLTAGS = ['img', 'input', 'br', 'hr',
+                  'wbr','meta','link', 'embed','basefont']
