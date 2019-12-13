5ff003e2162aac55a23fb309ffdeaeb9c4a9335a
Leonardo Barreiros <leossb36@gmail.com>
2019-12-13 11:28:09 -0300

    adicionando transformer para o parser

--- hyperfython/parser.py
+++ hyperfython/parser.py
@@ -7,13 +7,31 @@ from .runtime import Symbol
 class HTMLTransformer(InlineTransformer):
     
     def start(self, *args): 
-        return [Symbol.BEGIN, *args]
+        return [Symbol.BEGIN, *args] # begin == "fython"
+    
+    def block(self, *args):
+        return list(args) # args -> array de dict
+
+    def pair(self, key, value):
+        return (key, value)
+
+    def dictionary(self, *args):
+        return dict(args)
+
+    def atom(self, token):
+        try:
+            return str(token)
+        except ValueError:
+            try:
+                if token.type == 'SYMBOL':
+                    return 'SYMBOL'
+            except ValueError:
+                if token.type == 'STRING':
+                    return str(token[1:-1])
 
     
-def parse(src: int):
-    """
-    Compila string de entrada e retorna a S-expression equivalente.
-    """
+def parse(src: str):
+
     return parser.parse(src)
 
 
