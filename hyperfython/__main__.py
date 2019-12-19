4db5edddb2569784d7d360b8985f25d60b224202
codehg <hugoaraliveira@gmail.com>
2019-12-19 19:37:49 -0300

    altera logica

--- hyperfython/__main__.py
+++ hyperfython/__main__.py
@@ -1,33 +1,11 @@
-import click
-from pprint import pprint
-from .parser import parse as parse_src
-from .runtime import eval, env
-from . import __version__ as version
+from hyperfython import parser
 
+while True:
+    text = input('hyperfython >')
+    result, error = parse(text)
 
-@click.command()
-@click.argument('file', type=click.File(), required=False)
-@click.option('--parse', '-p', is_flag=True, help='Realiza análise sintática sem executar código.')
-def main(file, parse):
-    if file is None:
-        e = env()
-        while True:
-            src = input('HyperFython> ')
-            if not src and input('sair? [y/n]').lower() == 'y':
-                break
-            ast = parse_src(src)
-            try:
-                print(eval(ast, e))
-            except Exception as exc:
-                print(exc.__class__.__name__, exc)
-    else:
-        ast = parse_src(file.read())
-        if parse:
-            pprint(ast)
-        value = eval(ast, env())
-        if value is not None:
-            print(value)
-
+    if error: print(error.as_string())
+    else: print(result)
 
 if __name__ == '__main__':
     main()
