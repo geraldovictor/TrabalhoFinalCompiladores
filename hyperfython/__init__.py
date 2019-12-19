c80c717f63860fd1183fe78a07b0535ab4e5dcdd
codehg <hugoaraliveira@gmail.com>
2019-12-19 19:37:16 -0300

    altera dependencia

--- hyperfython/__init__.py
+++ hyperfython/__init__.py
@@ -2,8 +2,8 @@
 Hyperfython é um interpretador de linguagem html a fim de poder renderizar expressões em html
 """
 
-from .symbol import Symbol, var
 from .parser import parse
-from .runtime import eval, env, global_env
+from .runtime import eval_sexpr, env, global_env
+from .htmltags import HTMLTAGS, CLOSEDHTMLTAGS , ATRIBUTO
 
 __version__ = '0.1.0'
