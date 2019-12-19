0fe526baec808d6b78f50a418e88062b40cf4698
geraldovictor <geraldovictor@outlook.com>
2019-12-19 00:21:26 -0300

    Atualiza eval Co-authored-by: codehg <hugoaraliveira@gmail.com>

--- hyperfython/render.py
+++ hyperfython/render.py
@@ -1,56 +1,35 @@
-from collections import OrderedDict
-from htmltags import HTMLTAGS, CLOSEDHTMLTAGS
+from htmltags import HTMLTAGS, CLOSEDHTMLTAGS, ATRIBUTO
 
 
-def eval(htmlObj):
+def isTag(tag):
+    for i in HTMLTAGS:
+        if tag == i:
+           return 1
+    for i in CLOSEDHTMLTAGS:
+        if tag == i:
+           return 1
+    return 0
 
-    html = ""
-    for initalTag in htmlObj:
-        
-        for tag, values in initalTag.items():
+def isAtrib(atributo):
+  for i in ATRIBUTO:
+    if atributo == i:
+      return 1
+  return 0
 
-            # values is a dict
-            parsed_attributes = ""
-            parsed_content = ""
-            try: 
-                for attribute, attribute_value in values.items():
+def eval_sexpr(sexpr):
+    
+    # Finalmente, deve ser uma S-expression. Separamos o primeiro termo
+    # dos argumentos e avaliamos o resultado condicionalmente
+    for i in sexpr:
+      # if i[0] == 'lista':
+      
+      if i[0] == 'tag':
+        if (isTag(i[1][1]) == 0):
+          print('Tag -> {} não encontrada'.format(i[1][1]))
 
-                    if attribute == 'content':
-                        for obj in  attribute_value:
-                            for inner_attr, innter_attr_value in obj.items():
-                                if inner_attr == 'text':
-                                    parsed_content += innter_attr_value
-                                else:
-                                    parsed_content += eval(
-                                        [{inner_attr: innter_attr_value}, ]
-                                    )
-                    elif attribute == 'style':
-                        classList = []
-                        for classes, elements in attribute_value.items():
-                            classList.append(f'{classes}:{elements};')
-                        
-                        parsed_attributes += f'{attribute}=" {" ".join(classList)} "'
-                    else:
-                        parsed_attributes += f' {attribute}="{attribute_value}" '
-                
-                if tag in HTMLTAGS:
-                    html += f"<{tag} {parsed_attributes}>{parsed_content}</{tag}>"
-                elif tag in CLOSEDHTMLTAGS:
-                    html += f"<{tag} {parsed_attributes}>"
-                else:
-                    raise Exception(f"Syntax Error: tag {tag} not recognized")
-
-            except AttributeError:
-                raise AttributeError('Expected a dict, got a list')
-    return html
-
-
-## 1 - Aceitar mais de uma tag inicial
-## 2 - Permitir que o content aceite mais de uma entrada para a mesma tag (fazer uma lista de objetos) 
-
-# <div  class="text"  id="div-pai" style=" color:red; font-size:12px; ">conteudo interno da tag<p >texto de uma tag p</p></div>
-# <div  class="row"  id="row" ><div  class="col md-6 lg-6" ><p >Coluna 1</p></div><div  class="col md-6 lg-6" ><p >Coluna 2</p></div></div>
-# <div ><div ><div ></div></div></div>
-# <head ><p >TITLE</p><br ></head><body ><p >LINE 1</p><br ><p >LINE 2</p></body>
-
-# print(eval([{'div':{'content':[{'div':{'content':[{'div':{}}]}}]}}]))
+      if i[0] == 'atributo':
+        if (isAtrib(i[1][1]) == 0):
+           print('Atributo -> {} nao encontrado'.format(i[1][1]))
+      
+      # elif head == 'funcao':
+        # passe para a proxima lista
