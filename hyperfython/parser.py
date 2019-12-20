1c225ff24cff05e3fd90bfc29e2724aafd75b628
geraldovictor <geraldovictor@outlook.com>
2019-12-20 01:02:44 -0300

    Atualiza parser
    Co-authored-by: codehg <hugoaraliveira@gmail.com>

--- hyperfython/parser.py
+++ hyperfython/parser.py
@@ -1,6 +1,5 @@
 from lark import Lark, InlineTransformer
-from hyperfython import grammar
-# from pathlib import Path
+from pathlib import Path
 
 class SExprTransformer(InlineTransformer):
     def funcao(self, *args):
@@ -21,19 +20,21 @@ class SExprTransformer(InlineTransformer):
     def nomefora(self, x):
       return ['nomefora' , str(x)]
 
-    
 def parse(src: str):
+    """
+    Compila string de entrada e retorna a S-expression equivalente.
+    """
+    return parser.parse(src)
 
-    return grammar.parse(src)
 
+def _make_grammar():
+    """
+    Retorna uma gramática do Lark inicializada.
+    """
 
-# def _make_grammar():
-#     """
-#     Retorna uma gramática do Lark inicializada.
-#     """
-#     path = Path(__file__).parent / 'grammar.lark'
-#     with open(path) as fd:
-#         grammar = Lark(fd, parser='lalr', transformer=SExprTransformer())
-#     return grammar
+    path = Path(__file__).parent / 'grammar.lark'
+    with open(path) as fd:
+        grammar = Lark(fd, parser='lalr', transformer=SExprTransformer())
+    return grammar
 
-# parser = _make_grammar()
+parser = _make_grammar()
